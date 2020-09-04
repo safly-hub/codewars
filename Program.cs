bc0ff4b5a6907e4479444bba7da7b19fe84139c6
safly-hub <69162005+safly-hub@users.noreply.github.com>
2020-09-04 19:52:39 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string Greet(string name)
+    {
+        return $"Hello, {name} how are you doing today?";
+    }
+
+    public static string SwitchItUp(int number)
+    {
+        string[] numbers =
+                    { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten" };
+
+        return numbers[number];
+    }
+
+}
+
